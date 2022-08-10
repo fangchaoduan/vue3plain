678fe2b04122b6b1993cf4e0020eef91eb023a34
namefangchaoduan <namefangchaoduan@163.com>
2022-08-11 01:18:48 +0800

    ast语法树中parse()中处理表达式及元素标签的解析;表达式是先处理`{{`,之后是表达式字符串,之后去除`}}`,再把节点信息返回; 而元素标签是先处理`<标签名`,之后是`属性名=属性值`,之后去掉`/>`或`>`,返回标签信息,同时把子节点递归用parseChildren()当成新模板进行递归处理得到子节点数据,并挂载到元素标签信息上;

--- packages/compiler-core/dist/compiler-core.global.js
+++ packages/compiler-core/dist/compiler-core.global.js
@@ -2,7 +2,21 @@ var VueCompilerCore = (() => {
   var __defProp = Object.defineProperty;
   var __getOwnPropDesc = Object.getOwnPropertyDescriptor;
   var __getOwnPropNames = Object.getOwnPropertyNames;
+  var __getOwnPropSymbols = Object.getOwnPropertySymbols;
   var __hasOwnProp = Object.prototype.hasOwnProperty;
+  var __propIsEnum = Object.prototype.propertyIsEnumerable;
+  var __defNormalProp = (obj, key, value) => key in obj ? __defProp(obj, key, { enumerable: true, configurable: true, writable: true, value }) : obj[key] = value;
+  var __spreadValues = (a, b) => {
+    for (var prop in b || (b = {}))
+      if (__hasOwnProp.call(b, prop))
+        __defNormalProp(a, prop, b[prop]);
+    if (__getOwnPropSymbols)
+      for (var prop of __getOwnPropSymbols(b)) {
+        if (__propIsEnum.call(b, prop))
+          __defNormalProp(a, prop, b[prop]);
+      }
+    return a;
+  };
   var __export = (target, all) => {
     for (var name in all)
       __defProp(target, name, { get: all[name], enumerable: true });
@@ -34,6 +48,9 @@ var VueCompilerCore = (() => {
   }
   function isEnd(context) {
     const source = context.source;
+    if (context.source.startsWith(`</`)) {
+      return true;
+    }
     return !source;
   }
   function getCursor(context) {
@@ -83,35 +100,143 @@ var VueCompilerCore = (() => {
     }
     const start = getCursor(context);
     const content = parseTextData(context, endIndex);
-    return {
+    const theParseNode = {
       type: 2 /* TEXT */,
       content,
       loc: getSelection(context, start)
     };
+    return theParseNode;
+  }
+  function parseInterpolation(context) {
+    const start = getCursor(context);
+    const closeIndex = context.source.indexOf("}}", 2);
+    advanceBy(context, 2);
+    const innerStart = getCursor(context);
+    const innerEnd = getCursor(context);
+    const rawContentLength = closeIndex - 2;
+    const preContent = parseTextData(context, rawContentLength);
+    const content = preContent.trim();
+    const startOffset = preContent.indexOf(content);
+    if (startOffset > 0) {
+      advancePositionWithMutation(innerStart, preContent, startOffset);
+    }
+    const endOffset = startOffset + content.length;
+    advancePositionWithMutation(innerEnd, preContent, endOffset);
+    advanceBy(context, 2);
+    const theParseNode = {
+      type: 5 /* INTERPOLATION */,
+      content: {
+        type: 4 /* SIMPLE_EXPRESSION */,
+        content,
+        loc: getSelection(context, innerStart, innerEnd)
+      },
+      loc: getSelection(context, start)
+    };
+    return theParseNode;
+  }
+  function advanceBySpaces(context) {
+    const theRegExp = /^[ \t\r\n]+/;
+    const match = theRegExp.exec(context.source);
+    if (match) {
+      advanceBy(context, match[0].length);
+    }
+  }
+  function parseAttributeValue(context) {
+    const start = getCursor(context);
+    const quote = context.source[0];
+    let content;
+    if (quote === `"` || quote === `'`) {
+      advanceBy(context, 1);
+      const endIndex = context.source.indexOf(quote);
+      content = parseTextData(context, endIndex);
+      advanceBy(context, 1);
+    }
+    return {
+      content,
+      loc: getSelection(context, start)
+    };
+  }
+  function parseAttribute(context) {
+    const start = getCursor(context);
+    const match = /^[^\t\r\n\f />][^\t\r\n\f />=]*/.exec(context.source);
+    const name = match[0];
+    advanceBy(context, name.length);
+    advanceBySpaces(context);
+    advanceBy(context, 1);
+    const value = parseAttributeValue(context);
+    const theReturn = {
+      type: 6 /* ATTRIBUTE */,
+      name,
+      value: __spreadValues({
+        type: 2 /* TEXT */
+      }, value),
+      loc: getSelection(context, start)
+    };
+    return theReturn;
+  }
+  function parseAttributes(context) {
+    const props = [];
+    while (context.source.length > 0 && !context.source.startsWith(`>`) && !context.source.startsWith(`/>`)) {
+      const prop = parseAttribute(context);
+      props.push(prop);
+      advanceBySpaces(context);
+    }
+    return props;
+  }
+  function parseTag(context) {
+    const start = getCursor(context);
+    const theRegExp = /^[<][/]{0,1}([a-z][^ \t\r\n/>]*)/;
+    const match = theRegExp.exec(context.source);
+    const tag = match[1];
+    advanceBy(context, match[0].length);
+    advanceBySpaces(context);
+    const props = parseAttributes(context);
+    const isSelfClosing = context.source.startsWith(`/>`);
+    advanceBy(context, isSelfClosing ? 2 : 1);
+    const theReturn = {
+      type: 1 /* ELEMENT */,
+      tag,
+      isSelfClosing,
+      children: [],
+      props,
+      loc: getSelection(context, start)
+    };
+    return theReturn;
+  }
+  function parseElement(context) {
+    const ele = parseTag(context);
+    const children = parseChildren(context);
+    if (context.source.startsWith(`</`)) {
+      parseTag(context);
+    }
+    ele.loc = getSelection(context, ele.loc.start);
+    ele.children = children;
+    const theParseNode = ele;
+    return theParseNode;
   }
   function parse(template) {
     const context = createParserContext(template);
+    return parseChildren(context);
+  }
+  function parseChildren(context) {
     const nodes = [];
-    for (; !isEnd(context); ) {
+    while (!isEnd(context)) {
       const source = context.source;
       let node;
       if (source.startsWith("{{")) {
-        node = "xxx";
+        node = parseInterpolation(context);
       } else if (source[0] === "<") {
-        node = "qqq";
+        node = parseElement(context);
       }
       if (!node) {
         node = parseText(context);
       }
       nodes.push(node);
-      console.log(nodes);
-      break;
     }
-    return;
+    return nodes;
   }
   function compile(template) {
     const ast = parse(template);
-    console.log("ast--->", ast);
     return ast;
   }
   return __toCommonJS(src_exports);
--- packages/compiler-core/src/index.ts
+++ packages/compiler-core/src/index.ts
@@ -2,13 +2,19 @@
 
 import { NodeTypes } from "./ast";
 
-type TemplateContext = {
+/* type TemplateContext = {
   line: number;//第几行;
   column: number;//第几列;
   offset: number;//偏移量;
-  source: string;//当前要被解析模版,此字段会被不停地进行解析,会slice()一直被截短;
+  source: string;//当前要被解析模版,此字段会被不停地进行解析,会slice()一直被截短;//内容为空,说明已经解析结束;
   originalSource: string;//原模版,此字段是原模版,不会被解析;
-}
+} */
+
+type TemplateContext = {
+  source: string;//当前要被解析模版,此字段会被不停地进行解析,会slice()一直被截短;//内容为空,说明已经解析结束;
+  originalSource: string;//原模版,此字段是原模版,不会被解析;
+} & CursorObject
+//创建当前
 function createParserContext(template: string): TemplateContext {
   const theContext: TemplateContext = {
     line: 1,
@@ -23,23 +29,32 @@ function createParserContext(template: string): TemplateContext {
 
 function isEnd(context: TemplateContext) {
   const source = context.source
-  return !source;
+
+  //以闭合标签为开始时;
+  if (context.source.startsWith(`</`)) {
+    return true
+  }
+
+  return !source;//没有内容时;
 }
 
 //行列游标信息,用来报错之类的用到;
 type CursorObject = {
-  line: number;
-  column: number;
-  offset: number;
+  line: number;//第几行;
+  column: number;//第几列;
+  offset: number;//偏移量;
 }
+
 //得到当前上下文的游标信息,用于报错提示之类的;
+//getCursor 获取位置的信息,根据当前的上下文;
 function getCursor(context: TemplateContext): CursorObject {
   const { line, column, offset } = context
   return { line, column, offset }
 }
 
-//更新最新的行列和偏移量信息;
-function advancePositionWithMutation(context: TemplateContext, source: string, endIndex: number) {
+//更新最新的行列及偏移量信息及源内容和更新后内容;
+//advancePositionWithMutation 更新信息;
+function advancePositionWithMutation(context: CursorObject | TemplateContext, source: string, endIndex: number) {
   let linesCount = 0;
   let linePos = -1;
   for (let i = 0; i < endIndex; i++) {
@@ -61,8 +76,13 @@ function advancePositionWithMutation(context: TemplateContext, source: string, e
   //console.log('linePos--->', linePos)
   // debugger
 }
+/* 12312321231
+    123456789
+
+12312321231123456789    */
 
 //把当前上下文的当前模版在结束角标前的文本截去;
+//advanceBy 会进行前进删除;
 function advanceBy(context: TemplateContext, endIndex: number) {
   //每次删掉内容的时候,都要更新最新的行列和偏移量信息;
   const source = context.source
@@ -70,7 +90,9 @@ function advanceBy(context: TemplateContext, endIndex: number) {
   context.source = source.slice(endIndex)
 }
 
+
 //截取当前上下文的文本内容;
+//parseTextData 处理文本内容,并且会更新最新的偏移量信息;
 function parseTextData(context: TemplateContext, endIndex: number) {
   const rawText = context.source.slice(0, endIndex)
   advanceBy(context, endIndex)
@@ -78,11 +100,13 @@ function parseTextData(context: TemplateContext, endIndex: number) {
   return rawText
 }
 
+//位置信息;
 type SelectionObject = {
-  start: CursorObject;
+  start: CursorObject;//第几行;
   end: CursorObject;
   source: string;
 }
+//getSelection 获取当前开头和结尾的位置;
 function getSelection(context: TemplateContext, start: CursorObject, end?: CursorObject) {
   end = end || getCursor(context)
   //console.log(start.offset, end.offset)
@@ -93,7 +117,18 @@ function getSelection(context: TemplateContext, start: CursorObject, end?: Curso
   }
   return theReturn
 }
-function parseText(context: TemplateContext): any {
+
+type ParseNode = {
+  type: NodeTypes;//节点类型;
+  content?: string | ParseNode;//节点内容;
+  tag?: string;//标签类型;
+  isSelfClosing?: boolean;//是否标签自闭合;
+  children?: ParseNode[],//儿子节点;
+  props?: ElementProp[],//属性;
+  loc: SelectionObject;//位置信息;
+}
+
+function parseText(context: TemplateContext) {
   //在解析文本的;
   const endTokens = [`<`, `{{`]//结束标识列表;
   //asafs{{d<jfhlaksdjhalkjfhd;sjfh;
@@ -114,40 +149,260 @@ function parseText(context: TemplateContext): any {
   //取内容;
   const content = parseTextData(context, endIndex)
   //debugger
-  return {
+  const theParseNode: ParseNode = {
     type: NodeTypes.TEXT,//类型;
     content: content,//内容;
     loc: getSelection(context, start),//位置信息;
   }
 
+  return theParseNode
   //再获取结束的位置;
 
   //debugger
 }
 
 
+//getCursor 获取位置的信息,根据当前的上下文;
+//parseTextData 处理文本内容,并且会更新最新的偏移量信息;
+//advancePositionWithMutation 更新信息;
+//getSelection 获取当前开头和结尾的位置;
+//advanceBy 会进行前进删除;
+
+//处理表达式的信息;
+function parseInterpolation(context: TemplateContext) {
+  const start = getCursor(context)//`{{  xxx  }}`;
+  //const closeIndex = context.source.indexOf('}}', '{{'.length)//查找结束的大括号;
+  const closeIndex = context.source.indexOf('}}', 2)//查找结束的大括号;
+
+  advanceBy(context, 2);//`  xxx  }}`;
+
+  const innerStart = getCursor(context);//用来记录表达式去掉了`{{`时的游标;
+  const innerEnd = getCursor(context);//后面再: advancePositionWithMutation;
+
+  //拿到原始的内容;
+  const rawContentLength = closeIndex - 2;//原始内容的长度,这个是减去了`  xxx  }}`中的`}}`;
+  const preContent = parseTextData(context, rawContentLength)//`  xxx  `;//可以返回文本内容,并且是可以更新信息;//此时context为`}}`;
+  const content = preContent.trim()//`xxx`;
+  const startOffset = preContent.indexOf(content)
+
+  if (startOffset > 0) {
+    advancePositionWithMutation(innerStart, preContent, startOffset)
+  }
+  const endOffset = startOffset + content.length
+  advancePositionWithMutation(innerEnd, preContent, endOffset)
+
+  advanceBy(context, 2)
+
+  const theParseNode: ParseNode = {
+    type: NodeTypes.INTERPOLATION,//表达式;
+    content: {
+      type: NodeTypes.SIMPLE_EXPRESSION,
+      content,
+      loc: getSelection(context, innerStart, innerEnd)
+    },
+    loc: getSelection(context, start),//位置信息;
+  }
+
+  return theParseNode
+}
+
+//上下文去掉前方的空格等空字符;
+function advanceBySpaces(context: TemplateContext) {
+  //const theRegExp = /^[ \t\r\n]{1,}/
+  const theRegExp = /^[ \t\r\n]+/
+  const match = theRegExp.exec(context.source)
+  if (match) {
+    advanceBy(context, match[0].length)
+  }
+}
+
+function parseAttributeValue(context: TemplateContext) {
+  const start = getCursor(context)
+  const quote = context.source[0];
+  let content: string
+  if (quote === `"` || quote === `'`) {
+    // debugger
+    //有引号的;
+    advanceBy(context, 1)//去除前引号;
+    const endIndex = context.source.indexOf(quote)
+    content = parseTextData(context, endIndex)
+    advanceBy(context, 1)//去除后引号;
+  }/*  else {
+    //没引号的暂时不考虑;
+    const endIndex1 = context.source.indexOf(`/`)
+    const endIndex2 = context.source.indexOf(`>`)
+    const endIndex3 = context.source.indexOf(` `)
+    const endIndex4 = context.source.indexOf(`\n`)
+    const endIndex5 = context.source.indexOf(`\r`)
+    const endIndex = [endIndex1, endIndex2, endIndex3, endIndex4, endIndex5].sort()[0]
+    content = parseTextData(context, endIndex)
+  } */
+
+  return {
+    content,
+    loc: getSelection(context, start)
+  }
+}
+
+
+type ElementProp = {
+  type: NodeTypes;
+  name: string;//属性名称;
+  value: {
+    content: string;//属性值内容;
+    loc: SelectionObject;
+    type: NodeTypes;
+  };
+  loc: SelectionObject;
+}
+//处理单个节点;
+function parseAttribute(context: TemplateContext) {
+  const start = getCursor(context)
+
+  //属性的名字;
+  const match = /^[^\t\r\n\f />][^\t\r\n\f />=]*/.exec(context.source)
+
+  const name = match[0]
+
+  //单属性的 如disabeld`不考虑先; 考虑`a=1`这种;
+  advanceBy(context, name.length)//有可能 `a = 1`?也有可能`a = '1'`;也有可能`a = "1"`;
+  // debugger
+  advanceBySpaces(context)
+  advanceBy(context, 1)//删除等号?
+  const value = parseAttributeValue(context)
+
+  //debugger
+  const theReturn:ElementProp = {
+    type: NodeTypes.ATTRIBUTE,
+    name,
+    value: {
+      type: NodeTypes.TEXT,
+      ...value,
+    },
+    loc: getSelection(context, start)
+  }
+  return theReturn
+
+
+}
+//处理多个属性;
+function parseAttributes(context: TemplateContext) {//`a=1 b=2 >`;
+  const props:ElementProp[] = []
+
+  //目前不考虑如`<div asdsad=1/>` input这类标签了;
+  //无内容时停止循环,初始为`>`时停止循环;
+  while (context.source.length > 0 && !context.source.startsWith(`>`) && !context.source.startsWith(`/>`)) {
+    const prop = parseAttribute(context)
+    props.push(prop)
+    advanceBySpaces(context)
+  }
+
+
+  return props;
+}
+
+/* type ElementNode = {
+  type: NodeTypes;
+  tag: string;
+  isSelfClosing: boolean;
+  loc: SelectionObject;
+} */
+function parseTag(context: TemplateContext) {
+  //进入进来标签若为: `<div    />`;则先删除`<div`,再删除`    `,再删除`/>`;
+  const start = getCursor(context)
+  //正则可以在: [正则表达式示意图查看网站](https://regexper.cn/)上看;
+  //const theRegExp = /^<\/?([a-z][^ \t\r\n/>]*)/;
+  //const theRegExp = /^[<][/]{0,1}([a-z][^ \t\r\n/>]*)/;
+  const theRegExp = /^[<][/]{0,1}([a-z][^ \t\r\n/>]*)/;
+  const match = theRegExp.exec(context.source)
+  const tag = match[1];//标签名: div <div  aa=1  >;
+  advanceBy(context, match[0].length);//删除整个标签;
+  advanceBySpaces(context)
+
+  //标签有两种: `<div></div>` `<div/>`;
+  //可能有属性;
+  const props = parseAttributes(context)
+
+
+
+  const isSelfClosing = context.source.startsWith(`/>`)//是否是自闭合标签;
+  advanceBy(context, isSelfClosing ? 2 : 1)//自闭合标签删2个字符,非自闭合删除1个;
+
+  const theReturn: ParseNode = {
+    type: NodeTypes.ELEMENT,
+    tag: tag,
+    isSelfClosing,
+    children: [],
+    props,
+    loc: getSelection(context, start)
+  }
+
+  return theReturn
+
+  //`<div></div>`;
+
+}
+
+//处理标签的信息;
+function parseElement(context: TemplateContext) {
+  //解析标签: 有: `<br/>` `<div asdsad="333" disabled >  </div>`
+
+  //假设有: `<div></div>`;
+  const ele = parseTag(context)//先干掉`<div></div>`中的`<div>`;
+
+  //儿子;
+  const children = parseChildren(context)//处理儿子的时候,可能没有儿子;
+
+  //`</div>`;
+  if (context.source.startsWith(`</`)) {
+    parseTag(context)//直接干掉如: `<div></div>`中的`</div>`;
+  }
+
+  ele.loc = getSelection(context, ele.loc.start)//计算最新的位置信息;
+  ele.children = children;//挂载儿子节点;
+
+
+  const theParseNode: ParseNode = ele
+
+  return theParseNode
+}
+
+
+
 //将html语法转换成js语法;
+//因为vue有自己的指令,插槽,动态表示如{{}},所以vue自己实现了,也更详细及精细一些;
 function parse(template: string) {
   //创建一个解析的上下文来进行处理;
   const context = createParserContext(template)
   //debugger
 
+  return parseChildren(context)
+}
+
+function parseChildren(context: TemplateContext) {
   //< 元素;
   //{{}} 说明表达式;
   //其它就是文本;
   //目前不考虑其它;
-  const nodes = []
-
-  for (; !isEnd(context);) {
+  const nodes: ParseNode[] = []
+  while (!isEnd(context)) {
     const source = context.source
-    let node
+    let node: ParseNode
     if (source.startsWith('{{')) {
       //说明表达式;
-      node = 'xxx'
+      //{{  xxx   }};
+      /* {{  
+        
+        xxx   
+      
+      }}; */
+      node = parseInterpolation(context)
     } else if (source[0] === '<') {
       //元素-标签;
-      node = 'qqq'
+      //break
+      node = parseElement(context)
     }
+
     //文本;
     //暂时不考虑其它,如注释之类的;
     if (!node) {
@@ -155,19 +410,19 @@ function parse(template: string) {
       //break
     }
     nodes.push(node)
-    console.log(nodes)
-    break
+    //console.log(nodes)
+    //break
   }
   //console.log(nodes)
 
-  return
+  return nodes
 }
 
 export function compile(template: string) {
-  //将模板转成抽象语法树;
+  //将模板转成抽象语法树-ast语法树;
   const ast = parse(template);//这里需要将html语法转换成js语法;--编译原理;
 
-  console.log('ast--->', ast)
+  //console.log('ast--->', ast)
   return ast
   //对ast语法树进行一些预先处理;
   // transform(ast);//会生成一些信息;
